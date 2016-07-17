4949c4dad892f68b9411d284ca25877d822a1ede
paolo_cena <paolo.cena@mail.polimi.it>
2016-07-17 19:13:49 +0200

    who we are and assistance

---
+++ conndb/assistanceList.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "youcall";
+$password = "nicogay";
+
+if (!$conn = mysql_connect($servername, $username, $password)) {
+    echo 'Could not connect to mysql';
+    exit;
+}
+
+if (!mysql_select_db('my_youcall', $conn)) {
+    echo 'Could not select database';
+    exit;
+}
+
+$category=mysql_real_escape_string($_GET['category']);
+$sql='SELECT subcategory,subsubcategory,name FROM Assistance WHERE category="'.$category.'"';
+$result = mysql_query($sql,$conn);
+
+if (!$result) {
+    echo "DB Error, could not query the database\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
+
+$arr= array();
+while ($row = mysql_fetch_assoc($result)){
+	array_push($arr,$row);
+}
+echo json_encode($arr);
+mysql_free_result($result);
+?>
---
+++ js/assistanceList.js
@@ -0,0 +1,68 @@
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+var category;
+$( document ).ready(function() {
+	category=getParameterByName("category");
+var ajaxRequest;  // The variable that makes Ajax possible!
+               
+               try {
+                  // Opera 8.0+, Firefox, Safari
+                  ajaxRequest = new XMLHttpRequest();
+               }catch (e) {
+                  // Internet Explorer Browsers
+                  try {
+                     ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+                  }catch (e) {
+                     try{
+                        ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                     }catch (e){
+                        // Something went wrong
+                        alert("Your browser broke!");
+                        return false;
+                     }
+                  }
+               }
+               
+               // Create a function that will receive data 
+               // sent from the server and will update
+               // div section in the same page.
+				var x;
+               ajaxRequest.onreadystatechange = function(){
+                  if(ajaxRequest.readyState == 4){
+                     x= ajaxRequest.responseText;
+					 x=JSON.parse(x);
+					 var i,j,k;
+					 var prec;
+					 for(i=0;i<x.length;i++){
+						 if(prec!=x[i].subcategory)
+							$("#bar-menu").append("<li><a href='#"+x[i].subcategory+"'><div>"+x[i].subcategory+"</div></a></li>");
+						 prec=x[i].subcategory;
+						}
+					for(i=0;i<x.length;i=j){
+						$("#list").append("<h1 class='category' id='"+x[i].subcategory+"'>"+x[i].subcategory+"</h1>");
+						for(j=i;x[j].subcategory==x[i].subcategory;j=k){
+							$("#list").append("<h2 class='subcategory'>"+x[j].subsubcategory+"</h2>");
+							for(k=j;x[j].subsubcategory==x[k].subsubcategory;k++)
+								$("#list").append("<p class='link-assistance'><a href='#'>"+x[k].name+"</a></p>");
+							}
+						$("#list").append("<hr>");
+						}
+					$("#list").append("<hr>");
+					}
+               }
+               
+               // Now get the value from user and pass it to
+               // server script.
+               var queryString = "?category="+category;
+               ajaxRequest.open("GET", "../conndb/assistanceList.php" + queryString, true);
+               ajaxRequest.send(null); 
+			   
+});
